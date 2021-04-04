0ac920b977b7a4b60897de3cbd0d7eea6d5bb6f9
Gabriel Rodrigues <gabriel.morais@sankhya.com.br>
2021-04-04 19:09:24 -0300

    Configurando Eureka para host indefinido

--- payroll/src/main/java/com/gabriel/payroll/PayrollApplication.java
+++ payroll/src/main/java/com/gabriel/payroll/PayrollApplication.java
@@ -2,14 +2,14 @@ package com.gabriel.payroll;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 //na verdade esse projeto Payroll é um cliente Ribbon ele que vai chamar outro projeto (worker) e essa chamada é com balanceamento de carga
 //Balanceamento de cargas = ALTERNAR OS SERVICOS QUE É CHAMADO
+//@RibbonClient(name = "hr-worker") => nao precisa mais pois o EUREKA JA TEM BALANCEAMENTO DE CARGAS
 
-
-@RibbonClient(name = "hr-worker")
+@EnableEurekaClient
 @EnableFeignClients
 @SpringBootApplication
 public class PayrollApplication {
