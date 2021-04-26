2f00c59a13799f5aa466f2f2c3ca0b63a5fbd3e0
Gabriel Rodrigues <gabriel.morais@sankhya.com.br>
2021-04-25 23:34:12 -0300

    User Role many to many association

---
+++ hruser/src/main/java/com/gabriel/hruser/entities/User.java
@@ -0,0 +1,121 @@
+package com.gabriel.hruser.entities;
+
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="tb_user")
+public class User implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	private String email;
+	private String password;
+	
+	//Many to Many: precisa de tabela DE-PARA
+	//Só de User tendo os de Role
+	//Importante: Muitos para muitos porém NAO pode REPETIR TER MESMO USUÀRIO COM MESMO ROLE
+	// LIST: aceita repetição => ROLE: NAO ACEITA repetição => ai pos role pra forçar
+	@ManyToMany(fetch = FetchType.EAGER)//EAGER contrario lazy => já carrega dados dos perfils automaticamente com usuários e nao pedir pra vim depois
+	@JoinTable(name = "tb_user_role", //Nome Tabela de para
+		joinColumns = @JoinColumn(name="user_id"),//Nome chave desta entitade
+		inverseJoinColumns = @JoinColumn(name="role_id")//Nome chave outra entidade
+		)
+	private Set<Role> roles = new HashSet<>();
+	
+	public User() {
+		
+	}
+
+	public User(Long id, String name, String email, String password) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.password = password;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+
+	//Getter roles
+	public Set<Role> getRoles() {
+		return roles;
+	}
+
+	//HashCode and Equals
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+	
+	
+	
+
+}
