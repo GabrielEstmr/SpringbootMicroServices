2dc34078fdd5b0e395ecc0a84c59776c29978c79
Gabriel Rodrigues <gabriel.morais@sankhya.com.br>
2021-04-25 22:36:04 -0300

    Project HrUser Created

---
+++ hruser/src/test/java/com/gabriel/hruser/HruserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabriel.hruser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HruserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
